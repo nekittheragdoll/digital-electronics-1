25cd2fbeb6060ca38ba43d44d5fdca508eec1652
nekittheragdoll <mykyta.syskov@seznam.cz>
2023-03-14 12:43:31 +0100

    cp 06

---
+++ 06-counter/counter/counter.srcs/sources_1/new/top.vhd
@@ -0,0 +1,113 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 14.03.2023 12:01:15
+-- Design Name: 
+-- Module Name: top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top is
+    Port ( CLK100MHZ : in STD_LOGIC;
+           SW : in STD_LOGIC;
+           CA : out STD_LOGIC;
+           CB : out STD_LOGIC;
+           CC : out STD_LOGIC;
+           CD : out STD_LOGIC;
+           CE : out STD_LOGIC;
+           CF : out STD_LOGIC;
+           CG : out STD_LOGIC;
+           AN : out STD_LOGIC_VECTOR (7 downto 0);
+           BTNC : in STD_LOGIC;
+           LED : out STD_LOGIC_VECTOR (7 downto 0));
+end top;
+
+----------------------------------------------------------
+-- Architecture body for top level
+----------------------------------------------------------
+
+architecture behavioral of top is
+
+  -- 4-bit counter @ 250 ms
+  signal sig_en_250ms : std_logic;                    --! Clock enable signal for Counter0
+  signal sig_cnt_4bit : std_logic_vector(3 downto 0); --! Counter0
+
+begin
+
+  --------------------------------------------------------
+  -- Instance (copy) of clock_enable entity
+  --------------------------------------------------------
+  clk_en0 : entity work.clock_enable
+      generic map(
+          g_MAX => 25000000
+      )
+      port map(
+          clk => CLK100MHZ,
+          rst => BTNC,
+          ce  => sig_en_250ms
+      );
+
+  --------------------------------------------------------
+  -- Instance (copy) of cnt_up_down entity
+  --------------------------------------------------------
+  bin_cnt0 : entity work.cnt_up_down
+     generic map(
+          g_CNT_WIDTH => 4
+      )
+      port map(
+          clk => CLK100MHZ,
+          rst => BTNC,
+          en => sig_en_250ms,
+          cnt_up => SW,
+          cnt => sig_cnt_4bit
+      );
+
+  --------------------------------------------------------
+  -- Instance (copy) of hex_7seg entity
+  --------------------------------------------------------
+  hex2seg : entity work.hex_7seg
+      port map(
+          blank_i  => BTNC,
+          hex_i    => sig_cnt_4bit,
+          seg_o(6) => CA,
+          seg_o(5) => CB,
+          seg_o(4) => CC,
+          seg_o(3) => CD,
+          seg_o(2) => CE,
+          seg_o(1) => CF,
+          seg_o(0) => CG
+      );
+
+  --------------------------------------------------------
+  -- Other settings
+  --------------------------------------------------------
+  -- Connect one common anode to 3.3V
+  AN <= b"0000_0000";
+  
+  
+
+end architecture behavioral;
